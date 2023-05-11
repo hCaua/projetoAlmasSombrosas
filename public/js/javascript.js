049db745877fdf061b79668154e0e87be58cddc3
h.Cauã Gustavo <110063239+hCaua@users.noreply.github.com>
2023-05-11 15:54:11 -0300

    atualização javascript, css e index

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -2,22 +2,41 @@ var indiceTelaAtual = 1
 var indiceProximaTela = ""
 var indiceTelaAnterior = ""
 
-function dirDireita(){
-
-        var telaAtual = document.querySelector(`[data-indice="${indiceTelaAtual}"]`) 
-        indiceProximaTela = indiceTelaAtual + 1
-        var proximaTela = document.querySelector(`[data-indice="${indiceProximaTela}"]`)
-        if(indiceTelaAtual == 1 && indiceTelaAtual == 0){
-            indiceTelaAnterior = 0
-        }else{
-            indiceTelaAnterior = indiceTelaAtual - 1
-            var telaAnterior = document.querySelector(`[data-indice="${indiceTelaAnterior}"]`)
-        }
-        indiceTelaAtual = indiceProximaTela
-
-
-console.log(telaAnterior)
-console.log(telaAtual)
-console.log(proximaTela)
-
+var telas = []
+for (i = 1; i <= 3; i++){
+    var tela = document.querySelector(`[id="tela${i}"]`) 
+    
+    telas.push(tela)
 }
+
+
+function dirDireita(){
+    for (i = 1; i < telas.length; i++){
+        var tela = document.querySelector(`[id="tela${i}"]`) 
+
+        tela.add
+    }    
+}
+
+// function dirDireita(){
+
+//         var telaAtual = document.querySelector(`[data-indice="${indiceTelaAtual}"]`) 
+//         indiceProximaTela = indiceTelaAtual + 1
+//         var proximaTela = document.querySelector(`[data-indice="${indiceProximaTela}"]`)
+//         if(indiceTelaAtual == 1 && indiceTelaAtual == 0){
+//             indiceTelaAnterior = 0
+//         }else{
+//             indiceTelaAnterior = indiceTelaAtual - 1
+//             var telaAnterior = document.querySelector(`[data-indice="${indiceTelaAnterior}"]`)
+//         }
+//         indiceTelaAtual = indiceProximaTela
+
+
+// console.log(telaAnterior)
+// console.log(telaAtual)
+// console.log(proximaTela)
+
+// }
+
+
+
